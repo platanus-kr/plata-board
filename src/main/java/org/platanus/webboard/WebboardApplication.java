430727632cc903f13d91cef19abab864cfb13e85
platanus <platanus@canxan.com>
2021-07-22 13:03:22 +0900

    init

---
+++ src/main/java/org/platanus/webboard/WebboardApplication.java
@@ -0,0 +1,13 @@
+package org.platanus.webboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/platanus/webboard/WebboardApplicationTests.java
@@ -0,0 +1,13 @@
+package org.platanus.webboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
