f1524176576d7815e61943748b1edbef755433d7
platanus <platanus@canxan.com>
2022-08-19 23:11:23 +0900

    feat: Add CORS Config

---
+++ src/main/java/org/platanus/webboard/config/interceptor/CORSConfig.java
@@ -0,0 +1,20 @@
+package org.platanus.webboard.config.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig implements WebMvcConfigurer {
+
+    /**
+     * 프론트엔드를 위한 CORS 허용 (react, 3000)
+     *
+     * @param registry
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000");
+    }
+}
