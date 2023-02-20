7709c09584be08806571d46f7b26ba57f1b335ca
platanus <platanus.kr@gmail.com>
2023-02-20 16:59:13 +0900

    feat: feature/#90 add WebMvcConfigurationSupport

---
+++ src/main/java/org/platanus/webboard/config/security/CustomWebMvcConfig.java
@@ -0,0 +1,19 @@
+package org.platanus.webboard.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+@Configuration
+public class CustomWebMvcConfig extends WebMvcConfigurationSupport {
+	
+	/**
+	 * 지친 static 리소스를 위한 안식처
+	 * @param registry
+	 */
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+		super.addResourceHandlers(registry);
+	}
+}
