7831cc95438b99d1bfc3833924caf31679604d7e
platanus <platanus@canxan.com>
2023-03-04 22:36:56 +0900

    feat: feature/#100 Remove resourceChain()

--- src/main/java/org/platanus/webboard/config/security/CustomWebMvcConfig.java
+++ src/main/java/org/platanus/webboard/config/security/CustomWebMvcConfig.java
@@ -1,19 +1,18 @@
 package org.platanus.webboard.config.security;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
-@Configuration
+//@Configuration
 public class CustomWebMvcConfig extends WebMvcConfigurationSupport {
-	
-	/**
-	 * 지친 static 리소스를 위한 안식처
-	 * @param registry
-	 */
-	@Override
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
-		super.addResourceHandlers(registry);
-	}
+
+    /**
+     * 지친 static 리소스를 위한 안식처
+     *
+     * @param registry
+     */
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/");
+//        super.addResourceHandlers(registry);
+//    }
 }
